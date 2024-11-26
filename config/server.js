0dcab3fe06e7de00a0d9be832a3c6259596f8364
AhmadAlkhaled <almzrab@gmail.com>
2024-11-26 14:04:21 +0100

    Update Node.js and npm versions

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Standardhost
+  port: env.int('PORT', 1337), // Standardport
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'), // API-Schlüssel aus Umgebungsvariablen
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false), // Webhooks-Optionen
+  },
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false), // Falls du Cron-Jobs nutzen möchtest
   },
 });
